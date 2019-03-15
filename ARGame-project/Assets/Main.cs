dae572f16b489a97406dd03e8bbe2f9d230b1adb
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-03-15 01:37:18 +0100

    Begin gemaakt aan de puzzelcamera: deze kan nu draaien naar de speler. Ook houdt de puzzelcamera bij hoe vaak hij gedraaid is en beweegt het op de verticale as, alshof het zich op een schroefdraad bevindt.

---
+++ ARGame-project/Assets/Main.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class Main : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
+/*
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+}
+*/
