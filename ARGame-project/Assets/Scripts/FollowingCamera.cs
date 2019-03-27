f42af62296ffaeeb6007fe771e52f94b5d23d084
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-03-27 23:26:43 +0100

    Following camera now follows an object through a path which will make puzzles possible

---
+++ ARGame-project/Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    public float maxSpeed;
+    public float acceleration;
+
+    public Rigidbody rb;
+
+
+    public Transform toFollow;
+
+
+    private Vector3 velocity;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        MoveTowardsPosition(toFollow);
+       
+    }
+
+    private void MoveTowardsPosition(Transform target)
+    {
+        Vector3 targetDirection = (target.position - transform.position).normalized;
+        targetDirection = Vector3.Scale(targetDirection, new Vector3(1, 0, 1)); // Verticale rotatie telt niet mee
+
+        Vector3 targetVelocity = targetDirection * maxSpeed;
+
+        //velocity = Vector3.Lerp(velocity, targetVelocity, acceleration);
+        rb.AddForce(targetVelocity);
+    }
+
+
+}
