fbab4289aa4cca8e452e1b0d36d0234329d413c4
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-03-29 00:08:31 +0100

    Added animation controller for the popup

---
+++ ARGame-project/Assets/Scripts/PlacementPopup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementPopup : MonoBehaviour
+{
+    public Animator animator;
+
+    public void OpenPopup()
+    {
+        animator.SetBool("showPopup", true);
+    }
+
+    public void ClosePopup()
+    {
+        animator.SetBool("showPopup", false);
+    }
+}
